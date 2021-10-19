5c38251cd29785798ee4e96b6acfb5165081993a
yoon <yoondot05@gmail.com>
2021-10-20 06:21:28 +0900

    mobile ver
    mobile ver

---
+++ custom.js
@@ -0,0 +1,28 @@
+// trigger
+$(function(){
+    $('.trigger').click(function(){
+        $(this).toggleClass('active')
+        $('.gnb').toggleClass('active')
+    });
+    $('section, .menu a').click(function(){
+        $('.gnb').removeClass('active');
+        $('.trigger').removeClass('active');
+    });
+
+    // Smooth Scrolling 
+    $('.menu a, .gototop').click(function(e) {
+        $.scrollTo(this.hash || 0, 900) 
+    })
+
+    // change css with scroll 
+     $(window).scroll(function(){
+         if($(window).scrollTop() > 50){
+             $('header, .gototop').addClass('active')
+         } 
+         else {
+            $('header, .gototop').removeClass('active')
+         }
+     })
+
+     
+});
