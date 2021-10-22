97609dececb5e6b8346837fc4a4af2e2e25a6d4d
yoon <yoondot05@gmail.com>
2021-10-22 15:51:50 +0900

    mobile css
    mobile css

--- custom.js
+++ custom.js
@@ -2,11 +2,11 @@
 $(function(){
     $('.trigger').click(function(){
         $(this).toggleClass('active')
-        $('.gnb').toggleClass('active')
+        $('.gnb, .sns').toggleClass('active')
     });
     $('section, .menu a').click(function(){
-        $('.gnb').removeClass('active');
-        $('.trigger').removeClass('active');
+        $('.gnb, .trigger, .sns').removeClass('active');
+        // $('.trigger').removeClass('active');
     });
 
     // Smooth Scrolling 
@@ -17,21 +17,57 @@ $(function(){
     // change css with scroll 
      $(window).scroll(function(){
          if($(window).scrollTop() > 50){
-             $('header, .gototop').addClass('active')
+             $('header, .gototop, .logo').addClass('active');
          } 
          else {
-            $('header, .gototop').removeClass('active')
+            $('header, .gototop, .logo').removeClass('active')
          }
      })
 
+   // slick.js : history
+  $('.history_slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  })
+
     //  slick js 
-    $('.history_slider').slick({
+    $('.project_photo').slick({
         dots: true,
         infinite: true,
-        slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
+        cssEase: 'linear'
+
     })
 
      
